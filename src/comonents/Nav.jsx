ade8043dda4acf213c2830a53bae96d46fd12931
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2025-09-07 11:16:22 +0530

    made delete user  button working and added toaster to give alert to user

--- src/comonents/Nav.jsx
+++ src/comonents/Nav.jsx
@@ -1,6 +1,11 @@
 //rrd imports
 import { Form, NavLink } from 'react-router-dom'
 
+
+//library
+
+import { TrashIcon } from '@heroicons/react/16/solid'
+
 //Assets
 import logomark from '../assets/logomark.svg'
 
@@ -14,9 +19,14 @@ function Nav({userName}) {
         </NavLink>
         {
             userName && (
-                <Form method='post' action='/logout'>
+                <Form method='post' action='/logout' onSubmit={(event) => {
+                    if(!confirm("Delete user and all data?")){
+                        event.preventDefault()
+                    }
+                }}>
                     <button type='submit' className='btn btn--warning'>
                         <span>Delete User</span>
+                        <TrashIcon width={20}/>
                     </button>
                 </Form>
             )
