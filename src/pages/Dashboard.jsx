a4b972b8072d773e7312da41f2ec9baf36be4065
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2025-09-07 11:54:45 +0530

    made login page for users

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,11 +5,25 @@ import { useLoaderData, } from 'react-router-dom'
 //helper function
 import { fetchData } from '../helpers'
 import Intro from '../comonents/Intro'
+import { toast } from 'react-toastify'
 
 //loader 
 export function dashboardLoader() {
     const userName = fetchData("userName")
     return { userName}
+}
+
+//action
+
+export async function dashboardAction({request}) {
+    const data = await request.formData();
+    const formData = Object.fromEntries(data)
+    try {
+        localStorage.setItem("userName",JSON.stringify(formData.userName))
+        return toast.success(`Welcome ${formData.userName}`)
+    } catch (e){
+        throw new Error("There was a problem creating your account.")
+    }
 }
 
 
