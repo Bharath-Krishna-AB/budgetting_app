a123e416dd8fc25aa3cad7e70b4432b8e6657ee4
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2025-09-07 19:22:24 +0530

    added expense form submission and storing data

--- src/helpers.js
+++ src/helpers.js
@@ -27,6 +27,22 @@ export const createBudget = ({
 }
 
 
+//create expense
+export const createExpense = ({
+    name,amount ,budgetId
+}) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createAt: Date.now(),
+        amount: +amount,
+        budgetId: budgetId
+    }
+    const existingExpenses = fetchData("expenses") ?? [];
+    return localStorage.setItem("expenses",JSON.stringify([...existingExpenses,newItem]))
+}
+
+
 //delete item
 
 export const deleteItem = ({key}) =>{
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,10 +3,11 @@ import { useLoaderData, } from 'react-router-dom'
 
 
 //helper function
-import { createBudget, fetchData } from '../helpers'
+import { createBudget, createExpense, fetchData } from '../helpers'
 import Intro from '../components/Intro'
 import { toast } from 'react-toastify'
 import AddBudgetForm from '../components/AddBudgetForm'
+import AddExpenseForm from '../components/AddExpenseForm'
 
 //loader 
 export function dashboardLoader() {
@@ -32,7 +33,7 @@ export async function dashboardAction({request}) {
     }
 
     //createBudget
-        if (_action === 'createBudget'){
+    if (_action === 'createBudget'){
         try {
             createBudget({
                 name: values.newBudget,
@@ -41,6 +42,19 @@ export async function dashboardAction({request}) {
             return toast.success('Budget Created.')
         } catch (e){
             throw new Error("There was a problem creating your budget.")
+        }
+    }
+    //create Expense
+    if(_action === 'createExpense'){
+        try {
+            createExpense({
+                name: values.newExpense,
+                amount: values.newExpenseAmount,
+                budgetId: values.newExpenseBudget
+            })
+            return toast.success(`Expense ${values.newExpense} created!`)
+        }catch (e) {
+            throw new Error("There was a problem creating your expense.")
         }
     }
 
@@ -56,12 +70,23 @@ const Dashboard = () =>{
             <div className="dashboard">
                 <h1>Welcome back, <span className="accent">{userName}</span></h1>
                 <div className="grid-sm">
-                    {/* {budgets ? (): ()} */}
-                    <div className="grid-lg">
-                        <div className="flex-lg">
-                            <AddBudgetForm/>
+                    { 
+                    budgets && budgets.length > 0 ?
+                    (
+                        <div className="grid-lg">
+                            <div className="flex-lg">
+                                <AddBudgetForm/>
+                                <AddExpenseForm budgets={budgets}/>
+                            </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div className="grid-sm">
+                            <p>Personal budgeting is the secret to financial freedom.</p>
+                            <p>Create a budget to get started!</p>
+                            <AddBudgetForm />
+                        </div>
+                    )
+                    }
                 </div>
             </div>
         ) : (<Intro />)}
